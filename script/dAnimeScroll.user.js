f44f2df5a87bd64f7bf2cdbd88f14d87d6d73c65
chimaha <107383950+chimaha@users.noreply.github.com>
2023-07-25 09:36:17 +0900

    イベントがいくつも追加されるバグ修正

--- script/dAnimeScroll.user.js
+++ script/dAnimeScroll.user.js
@@ -3,10 +3,11 @@
 // @namespace   https://github.com/chimaha/dAnimeScroll
 // @match       https://animestore.docomo.ne.jp/animestore/*
 // @grant       none
-// @version     1.0.5
+// @version     1.1
 // @author      chimaha
 // @description dアニメストアの横スクロールを、Firefoxで使用できるようにします
 // @license     MIT license
+// @icon        https://animestore.docomo.ne.jp/favicon.ico
 // @compatible  firefox
 // @require     https://unpkg.com/scrollbooster@2/dist/scrollbooster.min.js
 // @downloadURL https://github.com/chimaha/dAnimeScroll/raw/main/script/dAnimeScroll.user.js
@@ -20,6 +21,8 @@
 const observer = new MutationObserver(records => {
     const slider = document.querySelectorAll(".p-slider");
     for (let i = 0; i < slider.length; i++) {
+        if (slider[i].classList.contains("ScrollBooster")) { continue }
+        slider[i].classList.add("ScrollBooster");
         new ScrollBooster({
             viewport: slider[i],
             scrollMode: 'native',
