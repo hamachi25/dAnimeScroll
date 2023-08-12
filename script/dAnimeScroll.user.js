a4673999bab29ab5d23a503d0a9e0b6d2ed395ca
chimaha <107383950+chimaha@users.noreply.github.com>
2023-08-13 08:18:08 +0900

    デフォルトの動作をpreventDefaultでキャンセルするのみに変更

--- script/dAnimeScroll.user.js
+++ script/dAnimeScroll.user.js
@@ -6,7 +6,7 @@
 // @match       https://animestore.docomo.ne.jp/animestore/CR/*
 // @match       https://animestore.docomo.ne.jp/animestore/ci_pc?workId=*
 // @grant       none
-// @version     2.0
+// @version     2.1
 // @author      chimaha
 // @description dアニメストアの横スクロールを、Firefoxで使用できるようにします
 // @license     MIT license
@@ -21,36 +21,21 @@
 
 "use strict";
 
-let sliderItem;
 const observer = new MutationObserver(() => {
-    const slider = document.querySelectorAll(".p-slider__itemList");
+    const sliders = document.querySelectorAll(".p-slider__itemList");
 
-    for (let i = 0; i < slider.length; i++) {
-        if (slider[i].dataset.addScroll == "true") { continue; }
-        slider[i].dataset.addScroll = "true";
+    for (const slider of sliders) {
+        if (slider.dataset.addScroll == "true") { continue; }
+        slider.dataset.addScroll = "true";
 
-        slider[i].addEventListener('mousedown', e => {
+        slider.addEventListener('mousedown', e => {
             if (e.button == 0) {
                 e.preventDefault();
-                sliderItem = slider[i];
-                sliderItem.dataset.down = "true";
-                sliderItem.dataset.x = e.clientX;
-                sliderItem.dataset.scrollleft = sliderItem.scrollLeft;
             } else if (e.button == 2) {
                 e.stopPropagation();
             }
         });
-        slider[i].addEventListener('mousemove', e => {
-            if (e.button == 0 && slider[i].dataset.down == "true") {
-                slider[i].scrollLeft = Number(slider[i].dataset.scrollleft) + Number(slider[i].dataset.x) - e.clientX;
-            }
-        });
     }
-    document.addEventListener('mouseup', e => {
-        if (e.button == 0) {
-            sliderItem.dataset.down = "false";
-        }
-    });
 });
 const config = { childList: true, subtree: true };
 observer.observe(document.body, config);
